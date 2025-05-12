2aea4dba05f63835ae40fadd69b5430c7a13d82e
carloslospinos <carlosbackdev@gmail.com>
2025-05-12 18:57:36 +0200

    perfil details

--- app/src/main/java/com/powerroutine/PerfilActivity.java
+++ app/src/main/java/com/powerroutine/PerfilActivity.java
@@ -1,6 +1,7 @@
 package com.powerroutine;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +9,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.powerroutine.Componets.Navegator;
+import com.powerroutine.Static.UserStatic;
+import com.powerroutine.model.UserModel;
+
 public class PerfilActivity extends AppCompatActivity {
+    private UserModel user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_perfil);
+
+        user= UserStatic.user;
+        ImageButton btnHome = findViewById(R.id.btnHome);
+        ImageButton btnPerfil = findViewById(R.id.btnPerfil);
+        ImageButton btnCalendar = findViewById(R.id.btnCalendar);
+        new Navegator(btnHome,btnPerfil,btnCalendar,this,"perfil");
+
 
     }
 }
